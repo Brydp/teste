90781faf09909b61cb8ad79e837a5d249697f357
Brydp <129889850+Brydp@users.noreply.github.com>
2023-04-05 13:52:50 -0300

    alterações

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ document.addEventListener( "click" , showcolor );
 document.getElementById( "botao" ).addEventListener( "click" , exibenome );
 document.getElementById( "btn-soma" ).addEventListener( "click" , soma );
 document.getElementById( "btn-dividir" ).addEventListener( "click" , soma );
-document.getElementById( "btn-soma" ).addEventListener( "click" , soma );
-document.getElementById( "btn-soma" ).addEventListener( "click" , soma );
+document.getElementById( "btn-multiplicar" ).addEventListener( "click" , soma );
+document.getElementById( "btn-subtrair" ).addEventListener( "click" , soma );
 
 function showcolor()
 {
@@ -27,5 +27,17 @@ function dividir()
     let num1 = document.getElementById( "num1" ).value;
     let num2 = document.getElementById( "num2" ).value;
     document.getElementById( "resultado2" ).innerHTML = parseFloat (num1) / parseFloat (num2);
+}
+function multiplicar()
+{
+    let num1 = document.getElementById( "num1" ).value;
+    let num2 = document.getElementById( "num2" ).value;
+    document.getElementById( "resultado3" ).innerHTML = parseFloat (num1) / parseFloat (num2);
+}
+function subtrair()
+{
+    let num1 = document.getElementById( "num1" ).value;
+    let num2 = document.getElementById( "num2" ).value;
+    document.getElementById( "resultado4" ).innerHTML = parseFloat (num1) / parseFloat (num2);
 }
 
