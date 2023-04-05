859d5c57ccbc2690ec1a45cb0e3a4d9fb6b21437
Brydp <129889850+Brydp@users.noreply.github.com>
2023-04-05 11:00:48 -0300

    alterações

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 document.addEventListener( "click" , showcolor );
 
+document.getElementById( "botao" ).addEventListener( "click" , exibenome );
+
 function showcolor()
 {
     document.body.style.backgroundColor = "red";
+}
+
+function exibenome()
+{
+    let usúario = 
 }
 
